1e62ce96585706966e103b7196503e13f0517088
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-11-28 09:45:02 +0000

    k

--- src/PoolDBOutputService.cc
+++ src/PoolDBOutputService.cc
@@ -7,14 +7,15 @@
 #include "CondCore/MetaDataService/interface/MetaData.h"
 #include "CondCore/DBCommon/interface/PoolStorageManager.h"
 #include "CondCore/DBCommon/interface/RelationalStorageManager.h"
-#include "CondCore/DBCommon/interface/ConnectionConfiguration.h"
-#include "CondCore/DBCommon/interface/SessionConfiguration.h"
+//#include "CondCore/DBCommon/interface/ConnectionConfiguration.h"
+//#include "CondCore/DBCommon/interface/SessionConfiguration.h"
 #include "CondCore/IOVService/interface/IOVService.h"
 #include "CondCore/IOVService/interface/IOVEditor.h"
 #include "CondCore/DBCommon/interface/AuthenticationMethod.h"
 #include "CondCore/DBCommon/interface/ConnectMode.h"
 #include "CondCore/DBCommon/interface/MessageLevel.h"
 #include "CondCore/DBCommon/interface/Exception.h"
+#include "CondCore/DBCommon/interface/ConfigSessionFromParameterSet.h"
 #include "CondCore/DBOutputService/interface/Exception.h"
 #include "serviceCallbackToken.h"
 #include <iostream>
@@ -33,7 +34,11 @@ cond::service::PoolDBOutputService::PoolDBOutputService(const edm::ParameterSet 
   }else{
     m_endOfTime=edm::IOVSyncValue::endOfTime().time().value();
   }
-  edm::ParameterSet connectionPset = iConfig.getParameter<edm::ParameterSet>("DBParameters");  
+  std::string connect=iConfig.getParameter<std::string>("connect");
+  m_session=new cond::DBSession(connect);
+  m_catalog=iConfig.getUntrackedParameter<std::string>("catalog","file::PoolFileCatalog.xml");
+  edm::ParameterSet connectionPset = iConfig.getParameter<edm::ParameterSet>("DBParameters"); 
+  ConfigSessionFromParameterSet configConnection(*m_session,connectionPset);
   typedef std::vector< edm::ParameterSet > Parameters;
   Parameters toPut=iConfig.getParameter<Parameters>("toPut");
   for(Parameters::iterator itToPut = toPut.begin(); itToPut != toPut.end(); ++itToPut) {
@@ -44,50 +49,6 @@ cond::service::PoolDBOutputService::PoolDBOutputService(const edm::ParameterSet 
   }
   iAR.watchPreProcessEvent(this,&cond::service::PoolDBOutputService::preEventProcessing);
   iAR.watchPostEndJob(this,&cond::service::PoolDBOutputService::postEndJob);
-  std::string connect=connectionPset.getParameter<std::string>("connect");
-  m_session=new cond::DBSession(connect);
-  m_catalog=connectionPset.getUntrackedParameter<std::string>("catalog","file::PoolFileCatalog.xml");
-  std::string authMethod=connectionPset.getUntrackedParameter<std::string>("authenticationMethod","XML");
-  int messageLevel=connectionPset.getUntrackedParameter<int>("messageLevel",0);
-  bool enableConnectionSharing=connectionPset.getUntrackedParameter<bool>("enableConnectionSharing",true);
-  int connectionTimeOut=connectionPset.getUntrackedParameter<int>("connectionTimeOut",600);
-  bool enableReadOnlySessionOnUpdateConnection=connectionPset.getUntrackedParameter<bool>("enableReadOnlySessionOnUpdateConnection",true);
-  bool loadBlobStreamer=connectionPset.getUntrackedParameter<bool>("loadBlobStreamer",false);
-  int connectionRetrialPeriod=connectionPset.getUntrackedParameter<int>("connectionRetrialPeriod",30);
-  int connectionRetrialTimeOut=connectionPset.getUntrackedParameter<int>("connectionRetrialTimeOut",180);
-  if(authMethod=="ENV"||authMethod=="Env"){
-    m_session->sessionConfiguration().setAuthenticationMethod(cond::Env);
-  }else{
-    m_session->sessionConfiguration().setAuthenticationMethod(cond::XML);
-  }  
-  switch (messageLevel) {
-  case 0 :
-    m_session->sessionConfiguration().setMessageLevel( cond::Error );
-      break;    
-  case 1:
-    m_session->sessionConfiguration().setMessageLevel( cond::Warning );
-    break;
-  case 2:
-    m_session->sessionConfiguration().setMessageLevel( cond::Info );
-    break;
-  case 3:
-    m_session->sessionConfiguration().setMessageLevel( cond::Debug );
-    break;  
-  default:
-    m_session->sessionConfiguration().setMessageLevel( cond::Error );
-  }
-  if(enableConnectionSharing){
-    m_session->connectionConfiguration().enableConnectionSharing();
-  }
-  m_session->connectionConfiguration().setConnectionTimeOut(connectionTimeOut);
-  if(enableReadOnlySessionOnUpdateConnection){
-    m_session->connectionConfiguration().enableReadOnlySessionOnUpdateConnections();
-  }
-  if(loadBlobStreamer){
-    m_session->sessionConfiguration().setBlobStreamer("");
-  }
-  m_session->connectionConfiguration().setConnectionRetrialPeriod(connectionRetrialPeriod);
-  m_session->connectionConfiguration().setConnectionRetrialTimeOut(connectionRetrialTimeOut);
 }
 
 std::string cond::service::PoolDBOutputService::tag( const std::string& EventSetupRecordName ){
@@ -108,7 +69,7 @@ cond::service::PoolDBOutputService::initDB()
   try{
     cond::MetaData metadata(*m_coraldb);
     cond::IOVService iovservice(*m_pooldb);
-    m_coraldb->connect(cond::ReadOnly);
+    m_coraldb->connect(cond::ReadWriteCreate);
     m_coraldb->startTransaction(true);
     for(std::map<size_t,cond::service::serviceCallbackRecord>::iterator it=m_callbacks.begin(); it!=m_callbacks.end(); ++it){
       std::string iovtoken;
