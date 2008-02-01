c5622c83646e20c3afd40bbd37266a1829da6f9e
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-02-01 15:49:39 +0000

    add query log for last entry

--- test/unittest_DBLogger.cpp
+++ test/unittest_DBLogger.cpp
@@ -5,7 +5,7 @@
 #include "CondCore/DBCommon/interface/Connection.h"
 #include "CondCore/DBCommon/interface/CoralTransaction.h"
 #include "CondCore/DBCommon/interface/TokenBuilder.h"
-//#include "CondCore/DBCommon/interface/TokenInterpreter.h"
+#include "CondCore/DBOutputService/interface/LogDBEntry.h"
 #include "CondCore/DBOutputService/interface/Logger.h"
 #include "CondCore/DBOutputService/interface/UserLogInfo.h"
 
@@ -22,10 +22,15 @@ int main(){
 	 "PedestalsRcd",
 	 0);
   std::string tok1=tk.tokenAsString();
-
+  tk.set("3E60FA40-D105-DA11-981C-000E0C4DE431",
+	 "CondFormatsCalibration",
+	 "Pedestals",
+	 "PedestalsRcd",
+	 1);
+  std::string tok2=tk.tokenAsString();
   std::string constr("sqlite_file:mylog.db");
   cond::DBSession* session=new cond::DBSession;
-  session->configuration().setMessageLevel( cond::Error );
+  session->configuration().setMessageLevel( cond::Debug );
   session->configuration().setAuthenticationMethod(cond::XML);
   cond::Connection con(constr,-1);
   session->open();
@@ -35,10 +40,30 @@ int main(){
   cond::Logger mylogger(&con);
   mylogger.getWriteLock();
   cond::service::UserLogInfo a;
+  a.provenance="me";
   mylogger.createLogDBIfNonExist();
   mylogger.logOperationNow(a,constr,tok1,"mytag1","runnumber",0);
   mylogger.logFailedOperationNow(a,constr,tok1,"mytag1","runnumber",1,"EOOROR");
+  mylogger.logOperationNow(a,constr,tok2,"mytag1","runnumber",1);
   mylogger.releaseWriteLock();
+  std::cout<<"about to lookup last entry"<<std::endl;
+  cond::LogDBEntry result;
+  mylogger. LookupLastEntryByProvenance("me",result);
+  std::cout<<"result \n";
+  std::cout<<"logId "<<result.logId<<"\n";
+  std::cout<<"destinationDB "<<result.destinationDB<<"\n";
+  std::cout<<"provenance "<<result.provenance<<"\n";
+  std::cout<<"comment "<<result.comment<<"\n";
+  std::cout<<"iovtag "<<result.iovtag<<"\n";
+  std::cout<<"iovtimetype "<<result.iovtimetype<<"\n";
+  std::cout<<"payloadIdx "<<result.payloadIdx<<"\n";
+  std::cout<<"payloadName "<<result.payloadName<<"\n";
+  std::cout<<"payloadToken "<<result.payloadToken<<"\n";
+  std::cout<<"payloadContainer "<<result.payloadContainer<<"\n";
+  std::cout<<"exectime "<<result.exectime<<"\n";
+  std::cout<<"execmessage "<<result.execmessage<<std::endl;
+  //cond::LogDBEntry result;
+  //mylogger. LookupLastEntryByProvenance("me",result);
   //coralTransaction.commit();
   con.disconnect();
   delete session;
